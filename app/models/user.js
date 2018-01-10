f524e0c0d1284415847973a9b7a30edb489f23b9
Activity <activityconsultorti@gmail.com>
2018-01-10 14:52:53 -0500

    Add the model to the proyect

---
+++ app/models/user.js
@@ -0,0 +1,24 @@
+//We are going to use  Moongose so here we import that
+const mongoose = require('mongoose');  
+
+
+//Here we connect to the database
+mongoose.connect('mongodb://localhost/users');
+
+var Schema = mongoose.Schema; 
+// create a schema
+    var userSchema = new Schema({
+        user: { type: String, required: true, unique: true },
+        password: { type: String, required: true, minlength: 5 },
+        name: String,
+        identification: { type: Number, unique: true },
+        mail: String,
+        admin: Boolean
+      });
+
+// the schema is useless so far
+// we need to create a model using it
+      var User = mongoose.model('User', userSchema);
+      
+ // make this available to our users in our Node applications
+      module.exports = User;
